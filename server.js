dbfe654d04e0446fca662f19768e6b502a57f6eb
Jhonathan Weber <jhonmw@gmail.com>
2023-11-11 11:00:01 -0300

    refined message errors

--- server.js
+++ server.js
@@ -18,42 +18,67 @@ server.post("/videos", async (request, response) => {
 
     return response.status(201).send();
   } catch (error) {
-    console.log(new Error(error.message));
     return {
       error: error.message,
-      message: "Abort execution: Post not created!",
+      message:
+        "Abort execution: Post not created,  please try again, if error persists contact administrator of this service",
     };
   }
 });
 
 server.get("/videos", async (request) => {
-  const search = request.query.search;
-  const videos = await db.list(search);
-  return videos;
+  try {
+    const search = request.query.search;
+    const videos = await db.list(search);
+    return videos;
+  } catch (error) {
+    return {
+      error: error.message,
+      message:
+        "Abort execution of search query,  please try again, if error persists contact administrator of this service",
+    };
+  }
 });
 
 server.put("/videos/:id", async (request, response) => {
-  const videoId = request.params.id;
-  const { title, description, duration } = request.body;
+  try {
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body;
 
-  await db.update(videoId, {
-    title,
-    description,
-    duration,
-  });
+    await db.update(videoId, {
+      title,
+      description,
+      duration,
+    });
 
-  return response.status(204).send();
+    return response.status(204).send();
+  } catch (error) {
+    return {
+      error: error.message,
+      message:
+        "Aborted video update, please try again, if error persists contact administrator of this service.",
+    };
+  }
 });
 
 server.delete("/videos/:id", async (request, response) => {
-  const videoId = request.params.id;
+  try {
+    const videoId = request.params.id;
 
-  await db.delete(videoId);
+    await db.delete(videoId);
 
-  return response.status(204).send();
+    return response.status(204).send();
+  } catch (error) {
+    return {
+      error: error.message,
+      message:
+        "Aborted video delete, please try again, if error persists contact administrator of this service.",
+    };
+  }
 });
 
 server.listen({
+  // eslint-disable-next-line no-undef
   port: process.env.PORT ?? 3333,
   host: "0.0.0.0",
 });
